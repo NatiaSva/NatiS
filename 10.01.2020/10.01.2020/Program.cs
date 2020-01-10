ae8f6a8231746a291430fa5959e0a2db1c7b52e1
NatiaSva <55664887+NatiaSva@users.noreply.github.com>
2020-01-10 22:18:02 +0200

    1

---
+++ 10.01.2020/10.01.2020/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._01._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 54
+            Point a, b;
+            a.x = 10;
+            b.x = 5;
+            a.y = 20;
+            b.y = 15;
+            Console.WriteLine("GetDistance={0}",GetDistance(a, b));
+
+
+            Console.ReadKey();
+        }
+
+        static int Sqrt(int x)
+        {
+            int result = 0;
+            while (result * result < x)
+            {
+                result++;
+            }
+            return result;
+        }
+
+        //Exercise 54
+        static double GetDistance(Point a,Point b)
+        {
+            double distance;
+            distance = Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+            return distance;
+        }
+    }
+
+    //Exercise 54
+    struct Point
+    {
+        public int x, y;
+    }
+}
